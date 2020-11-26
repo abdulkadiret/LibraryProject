cb33083d8e0586375e2f70c26e033b6f0916f2c3
kchuc <kchuc@DESKTOP-TCGULBN>
2020-11-26 23:30:33 +0000

    Akey, this is required to create the Library constructor (for reference)
    https://stackoverflow.com/questions/17240708/implicit-super-constructor-object-is-undefined-must-explicitly-invoke-another/19570719
    
    What you have in this push is the skeleton of the programme, the rest is
    just about adding flesh (functions). It's basically the Library box from
    the Library_Project_Master (page 4) diagram. I will double check it with
    my friend tomorrow but I think it's a good start. I think it will be
    important to follow the development order starting from Member (add and
    get).
    
    I don't know what aggregation and inheritance signs mean in practice on
    the diagram
    
    https://www.google.com/url?sa=i&url=https%3A%2F%2Fen.wikipedia.org%2Fwiki%2FClass_diagram&psig=AOvVaw1phKjDM0mIXBV6NGZwJ97M&ust=1606519748534000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCPizx8-uoe0CFQAAAAAdAAAAABAD

--- src/Library.java
+++ src/Library.java
@@ -1,4 +1,58 @@
+import java.util.List;
 
-public class Library {
+public class Library 
+{
+	Library HendersonDuffy = new Library();
+
+	List<Member>members;
+
+	List<Stock>stock;
+
+	void addMember(String nm, int yr, String St, String Twn, String strpc) 
+	{
+		//insert addMember function here?
+	}
+
+	String getMembers()
+	{
+		//insert getMembers function here?
+	}
+
+	void addJournal(int v, String t, int numCopies)
+	{
+		//insert addJournal here?
+	}
+
+	void addBook(String a, String t, int numCopes)
+	{
+		//insert addBook?
+	}
+
+	String getStock()
+	{
+		//insert getStock function here?
+	}
+
+	String getBooks()
+	{
+		//insert getBooks function here?
+	}
+
+	String getJournals()
+	{
+		//insert getJournals function here?
+	}
+
+
+
+
+
+
+
+
+
+}
+
+
 
 }
